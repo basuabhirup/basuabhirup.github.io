08a78bba4091a9506d8eeec64eb235856cbb71ac
basuabhirup <basu.abhirup@yahoo.com>
2021-10-01 21:31:12 +0530

    gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Navbar from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero"
+import AboutMe from "./components/AboutMe/AboutMe"
+import Skills from "./components/Skills/Skills"
+import Projects from "./components/Projects/Projects"
+import Footer from "./components/Footer/Footer"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Hero />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
