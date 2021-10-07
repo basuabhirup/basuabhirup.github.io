ecf0e74db48dbe4c26ec99bd554806d62a2da8b1
basuabhirup <basu.abhirup@yahoo.com>
2021-10-07 23:25:22 +0530

    Set Page Layout and design Hero component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React, {useState, useEffect} from "react";
 import './App.css';
-import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import AboutMe from "./components/AboutMe/AboutMe"
 import Skills from "./components/Skills/Skills"
@@ -8,9 +8,48 @@ import Footer from "./components/Footer/Footer"
 
 
 function App() {
+  const [minH, setMinH] = useState(window.innerHeight
+      || document.documentElement.clientHeight
+      || document.body.clientHeight);
+  
+  const [minW, setMinW] = useState(window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth);
+
+  function updateSize() {
+    setMinH(
+        window.innerHeight 
+          || document.documentElement.clientHeight
+          || document.body.clientHeight
+      )
+
+    setMinW(
+      window.innerWidth 
+        || document.documentElement.clientWidth
+        || document.body.clientWidth
+    )
+    
+    document.querySelector('.app > *:first-child').style.minHeight = `${minH}px` || `100vh`;
+    document.querySelectorAll('.app > *:nth-child(n)').forEach(element => {
+      element.style.minWidth = `${.95 * minW}px` || `95vw`;
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('load', updateSize);
+    window.addEventListener('resize', updateSize);
+    window.addEventListener('click', updateSize);
+    window.addEventListener('scroll', updateSize);
+    return () => {
+      window.removeEventListener('load', updateSize);
+      window.removeEventListener('resize', updateSize);
+      window.removeEventListener('click', updateSize);
+      window.removeEventListener('scroll', updateSize);
+    }
+  })
+  
   return (
-    <div className="App">
-      <Navbar />
+    <div className="app" onLoad={() => updateSize()}>
       <Hero />
       <AboutMe />
       <Skills />
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from 'react';
+import "./Hero.css";
+import svg from "./hero.svg";
+
 
 function Hero() {
+  
   return (
-    <div>
-      <p>Hero COmponent</p>
-    </div>
+    <section id="hero">
+      <nav className="navbar navbar-light navbar-expand-xl">
+        <div className="container-xxl">
+          <button className="navbar-toggler" name="button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#about-me">About Me</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">Skills</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">Projects</a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#cta">Contact Me</a>
+              </li>
+            </ul>  
+          </div>       
+        </div>
+      </nav>
+      <section className="hero">
+        <img src={svg} alt="Hero" />
+        <h1>Abhirup Basu</h1>
+        <h2>Web Developer</h2>
+        <a className="btn btn-primary" 
+          href="https://drive.google.com/file/d/1APo04SQrlhSFHJlkF9mwgPMxXE7YQVFh/view?usp=sharing"
+          target="_blank" rel="noreferrer"
+        > 
+          My Resume &nbsp; <i class="bi bi-box-arrow-up-right"></i>
+        </a>
+        <div className="social-icons">
+          <a href="https://github.com/basuabhirup" target="_blank" rel="noreferrer"><i class="bi bi-github" /></a>
+          <a href="https://twitter.com/basu_abhirup27" target="_blank" rel="noreferrer"><i class="bi bi-twitter" /></a>
+          <a href="https://www.linkedin.com/in/basu-abhirup/" target="_blank" rel="noreferrer"><i class="bi bi-linkedin" /></a>
+        </div>
+      </section>
+    </section>
   )
 }
 
