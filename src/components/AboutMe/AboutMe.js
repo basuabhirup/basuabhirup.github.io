27142f1cb2a8c2d803b60f11a169efbd8e7220e8
basuabhirup <basu.abhirup@yahoo.com>
2021-10-08 13:44:59 +0530

    Design AboutMe component

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./AboutMe.css"
 
 function AboutMe() {
   return (
-    <div>
-      <p>AboutMe Component</p>
+    <div id="about-me">
+    <img 
+      src="https://avatars.githubusercontent.com/u/69730155?s=400&u=33dd33c534d00d31eca19b7ce2d9eea8cfbf3a37&v=4"
+      alt="Abhirup Basu"         
+    />
+    <h2>Hello! I am Abhirup.</h2>
+    <p>I'm a passionate programmer and web developer with working experience and proficiency in building full-stack web applications using ReactJS, NodeJS, ExpressJS, MongoDB and some other frameworks. I love hot coffee, music, exploring new places and meeting new people.</p>
     </div>
   )
 }
